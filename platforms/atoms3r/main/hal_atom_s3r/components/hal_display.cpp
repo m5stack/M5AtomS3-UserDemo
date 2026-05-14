f3c78783517fd3e7f93b039f3d7f7f739506522c
Dylan Luo <dylan.luo@m5stack.com>
2026-05-14 15:49:30 +0800

    support ST7735S and GC9107 panels

--- platforms/atoms3r/main/hal_atom_s3r/components/hal_display.cpp
+++ platforms/atoms3r/main/hal_atom_s3r/components/hal_display.cpp
@@ -4,14 +4,18 @@
  * SPDX-License-Identifier: MIT
  */
 // Ref:  https://github.com/lovyan03/LovyanGFX/blob/master/examples/HowToUse/2_user_setting/2_user_setting.ino
+// Panel autodetect aligned with M5GFX M5AtomS3R (ESP32-S3 LGA56): ST7735S vs GC9107
 #include "../hal_atom_s3r.h"
 #include "../hal_config.h"
 #include <mooncake.h>
-// #include <lgfx/v1/panel/Panel_ST7789.hpp>
+#include <lgfx/v1/panel/Panel_ST7735.hpp>
 #include <lgfx/v1/panel/Panel_GC9A01.hpp>
 #include "../utils/lp5562/lp5562.h"
 #include "spdlog/spdlog.h"
 #include <M5UnitOLED.h>
+
+#include <driver/spi_master.h>
+#include <memory>
 
 /* -------------------------------------------------------------------------- */
 /*                                  Backlight                                 */
@@ -23,7 +27,6 @@ public:
     bool init(uint8_t brightness) override
     {
         spdlog::info("lcd backlight init");
-        // assert(__get_led_controller() != nullptr);
         return true;
     }
 
@@ -37,62 +40,138 @@ public:
     }
 };
 
+namespace {
+
+void pin_level(int_fast16_t pin, bool level)
+{
+    lgfx::pinMode(pin, lgfx::pin_mode_t::output);
+    if (level) {
+        lgfx::gpio_hi(pin);
+    } else {
+        lgfx::gpio_lo(pin);
+    }
+}
+
+void pin_reset(int_fast16_t pin, bool use_reset)
+{
+    lgfx::gpio_hi(pin);
+    lgfx::pinMode(pin, lgfx::pin_mode_t::output);
+    lgfx::delay(1);
+    if (!use_reset) {
+        return;
+    }
+    lgfx::gpio_lo(pin);
+    lgfx::delay(2);
+    lgfx::gpio_hi(pin);
+    lgfx::delay(10);
+}
+
+uint32_t read_panel_id(lgfx::Bus_SPI* bus, int_fast16_t pin_cs, uint32_t cmd = 0x04, uint8_t dummy_read_bit = 1)
+{
+    bus->beginTransaction();
+    pin_level(pin_cs, true);
+    bus->writeCommand(0, 8);
+    bus->wait();
+    pin_level(pin_cs, false);
+    bus->writeCommand(cmd, 8);
+    bus->beginRead(dummy_read_bit);
+    uint32_t res = bus->readData(32);
+    bus->endTransaction();
+    pin_level(pin_cs, true);
+    spdlog::debug("LCD panel ID (cmd 0x{:02x}): 0x{:08x}", (unsigned)cmd, (unsigned)res);
+    return res;
+}
+
+}  // namespace
+
 /* -------------------------------------------------------------------------- */
 /*                                    Panel                                   */
 /* -------------------------------------------------------------------------- */
 
 class LGFX_AtomS3R : public lgfx::LGFX_Device {
-    lgfx::Panel_GC9107 _panel_instance;
     lgfx::Bus_SPI _bus_instance;
     Light_AtomS3R _light_instance;
+    std::unique_ptr<lgfx::Panel_Device> _panel_owned;
 
 public:
     LGFX_AtomS3R(void)
     {
-        {
-            auto cfg = _bus_instance.config();
+        pin_reset(HAL_PIN_LCD_RST, true);
 
-            cfg.pin_mosi = HAL_PIN_LCD_MOSI;
-            cfg.pin_miso = HAL_PIN_LCD_MISO;
-            cfg.pin_sclk = HAL_PIN_LCD_SCLK;
-            cfg.pin_dc   = HAL_PIN_LCD_DC;
-            // cfg.freq_write = 4000000;
-            // cfg.freq_write = 10000000;
-            cfg.freq_write = 40000000;
-            cfg.spi_host   = SPI2_HOST;
+        auto bus_cfg        = _bus_instance.config();
+        bus_cfg.pin_mosi    = HAL_PIN_LCD_MOSI;
+        bus_cfg.pin_miso    = (gpio_num_t)HAL_PIN_LCD_MISO;
+        bus_cfg.pin_sclk    = HAL_PIN_LCD_SCLK;
+        bus_cfg.pin_dc      = HAL_PIN_LCD_DC;
+        bus_cfg.spi_mode    = 0;
+        bus_cfg.spi_3wire   = true;
+        bus_cfg.freq_write  = 8000000;
+        bus_cfg.freq_read   = 8000000;
+        bus_cfg.spi_host    = SPI2_HOST;
+        bus_cfg.dma_channel = SPI_DMA_CH_AUTO;
+        bus_cfg.use_lock    = true;
+        _bus_instance.config(bus_cfg);
+        _bus_instance.init();
 
-            _bus_instance.config(cfg);
-            _panel_instance.setBus(&_bus_instance);
-        }
-        {
-            auto cfg = _panel_instance.config();
+        const uint32_t id    = read_panel_id(&_bus_instance, HAL_PIN_LCD_CS);
+        const bool is_st7735 = ((id & 0xFFFF) == 0x7683 || (id & 0xFFFF) == 0x897C);
+        const bool is_gc9107 = (id & 0xFFFFFF) == 0x079100;
 
-            cfg.invert    = true;
-            cfg.rgb_order = true;
-            cfg.pin_cs    = HAL_PIN_LCD_CS;
-            cfg.pin_rst   = HAL_PIN_LCD_RST;
-            cfg.pin_busy  = HAL_PIN_LCD_BUSY;
-
-            cfg.panel_width      = 128;
-            cfg.panel_height     = 128;
-            cfg.offset_x         = 0;
-            cfg.offset_y         = 32;
-            cfg.offset_rotation  = 0;
-            cfg.dummy_read_pixel = 8;
-            cfg.dummy_read_bits  = 1;
-            cfg.readable         = true;
-            cfg.invert           = false;
-            cfg.rgb_order        = false;
-            cfg.dlen_16bit       = false;
-            cfg.bus_shared       = true;
-
-            _panel_instance.config(cfg);
-        }
-        {  // バックライト制御の設定を行います。（必要なければ削除）
-            _panel_instance.setLight(&_light_instance);  // バックライトをパネルにセットします。
+        if (!is_st7735 && !is_gc9107) {
+            spdlog::error("AtomS3R LCD: unknown panel ID 0x{:08x} (expected ST7735S or GC9107)", (unsigned)id);
+            _bus_instance.release();
+            setPanel(nullptr);
+            return;
         }
 
-        setPanel(&_panel_instance);
+        _bus_instance.release();
+        bus_cfg.spi_host   = SPI3_HOST;
+        bus_cfg.freq_write = 40000000;
+        bus_cfg.freq_read  = 16000000;
+        bus_cfg.spi_3wire  = true;
+        _bus_instance.config(bus_cfg);
+        _bus_instance.init();
+
+        if (is_st7735) {
+            spdlog::info("AtomS3R LCD: ST7735S");
+            auto* p = new lgfx::Panel_ST7735S();
+            p->bus(&_bus_instance);
+            auto cfg            = p->config();
+            cfg.pin_cs          = HAL_PIN_LCD_CS;
+            cfg.pin_rst         = HAL_PIN_LCD_RST;
+            cfg.panel_width     = 128;
+            cfg.panel_height    = 128;
+            cfg.offset_x        = 2;
+            cfg.offset_y        = 31;
+            cfg.offset_rotation = 2;
+            cfg.readable        = true;
+            cfg.bus_shared      = false;
+            cfg.invert          = true;
+            p->config(cfg);
+            _panel_owned.reset(p);
+        } else {
+            spdlog::info("AtomS3R LCD: GC9107");
+            auto* p = new lgfx::Panel_GC9107();
+            p->bus(&_bus_instance);
+            auto cfg         = p->config();
+            cfg.pin_cs       = HAL_PIN_LCD_CS;
+            cfg.pin_rst      = HAL_PIN_LCD_RST;
+            cfg.panel_width  = 128;
+            cfg.panel_height = 128;
+            cfg.offset_y     = 32;
+            cfg.readable     = false;
+            cfg.bus_shared   = false;
+            p->config(cfg);
+            _panel_owned.reset(p);
+        }
+
+        _panel_owned->setLight(&_light_instance);
+        setPanel(_panel_owned.get());
+    }
+
+    ~LGFX_AtomS3R() override
+    {
+        setPanel(nullptr);
     }
 };
 
@@ -105,7 +184,6 @@ void HAL_AtomS3R::disp_init()
     if (!_data.display->init()) {
         delete (_data.display);
         _data.display = nullptr;
-        // popFatalError("display init failed");
         spdlog::warn("display init failed");
     }
 
@@ -115,23 +193,16 @@ void HAL_AtomS3R::disp_init()
         _data.canvas->createSprite(_data.display->width(), _data.display->height());
     }
 
-    // Unit oled for factory test
     assert(_data.unit_oled == nullptr);
     _data.unit_oled = new M5UnitOLED(HAL_PIN_I2C_EXTER_SDA, HAL_PIN_I2C_EXTER_SCL, 400000, 1);
     if (!_data.unit_oled->init()) {
         delete _data.unit_oled;
         _data.unit_oled = nullptr;
-        // popFatalError("unit oled init failed");
         spdlog::warn("unit oled init failed");
     } else {
         _data.unit_oled->setRotation(1);
         _data.unit_oled->fillScreen(TFT_WHITE);
     }
-
-    /* -------------------------------------------------------------------------- */
-    /*                                    Test                                    */
-    /* -------------------------------------------------------------------------- */
-    // _disp_test();
 }
 
 void HAL_AtomS3R::disp_test()
